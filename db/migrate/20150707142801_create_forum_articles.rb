448cf67b146bb7dbdfc233d218b41a048e69656b
Nitza Alfinas <nitzaalfinas@gmail.com>
2015-09-10 07:39:55 +0700

    file migrasi baru dan mengganti url forum

---
+++ db/migrate/20150707142801_create_forum_articles.rb
@@ -0,0 +1,16 @@
+class CreateForumArticles < ActiveRecord::Migration
+  def change
+    create_table :forum_articles do |t|
+      t.integer :forum_topic_flag
+      t.integer :forum_parent_id
+      t.string :forum_title
+      t.text :forum_body
+      t.integer :member_id
+      t.string :forum_permalink
+      t.integer :forum_category_id
+      t.integer :forum_hit
+
+      t.timestamps null: false
+    end
+  end
+end
