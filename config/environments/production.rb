ae881334ab2eed4019be22c2c0caf71e82422b74
Nitza Alfinas <nitzaalfinas@gmail.com>
2015-09-12 07:39:32 +0700

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,13 @@ Rails.application.configure do
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'indorails.com'
+  }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   
   
