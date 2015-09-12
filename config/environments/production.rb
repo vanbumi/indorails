ba9d92a41418021ffbaab0a89ecabcd9ec5446e2
Nitza Alfinas <nitzaalfinas@gmail.com>
2015-09-12 07:25:17 +0700

    modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   
   
